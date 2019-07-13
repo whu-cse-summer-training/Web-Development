6745a3cbcf4bf43715e92afe9a1e9877e5bdbe4a
Shanghai-Patriarch <52645058+Shanghai-Patriarch@users.noreply.github.com>
2019-07-13 11:38:28 +0900

    给数据库里又加了点东西，未测试

--- back_end/content/models.py
+++ back_end/content/models.py
@@ -1,62 +1,61 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class Tag(models.Model):
-    name = models.CharField(_('Tag名称'), max_length = 50)
+    name = models.CharField('Tag名称', max_length = 50)
 
     def __str__(self):
         return self.name
 
 class Question(models.Model):
-    caption = models.CharField(_('问题标题'), max_length = 100)
-    description = models.TextField(_('问题详细描述'),)
+    caption = models.CharField('问题标题', max_length = 100)
+    description = models.TextField('问题详细描述')
     author = models.ForeignKey('users.User', on_delete = models.PROTECT)
-    modified_time = models.DateTimeField(_('最后编辑时间'), auto_now = True)
+    modified_time = models.DateTimeField('最后编辑时间', auto_now = True)
     tag = models.ManyToManyField(Tag, blank = True)
 
     def __str__(self):
         return self.caption
 
 class Answer(models.Model):
-    content = models.TextField(_('回答正文'),)
+    content = models.TextField('回答正文')
     author = models.ForeignKey('users.User', on_delete = models.PROTECT)
     question = models.ForeignKey(Question, on_delete = models.PROTECT)
-    modified_time = models.DateTimeField(_('最后编辑时间'), auto_now = True)
-    good = models.IntegerField(_('*好*'), default = 0)
-    bad = models.IntegerField(_('*不*'), default = 0)
+    modified_time = models.DateTimeField('最后编辑时间', auto_now = True)
+    good = models.IntegerField('*好*', default = 0)
+    bad = models.IntegerField('*不*', default = 0)
 
     def __str__(self):
-        return self.author.__str__() + '在问题”' + self.question.__str__() + '”下的回答'
+        return self.author.__str__() + ' 在问题”' + self.question.__str__() + '”下的回答'
 
 class CommentOfQuestion(models.Model):
-    content = models.TextField(_('评论内容'),)
+    content = models.TextField('评论内容',)
     author = models.ForeignKey('users.User', on_delete = models.PROTECT)
-    created_time = models.DateTimeField(_('评论时间'), auto_now_add = True)
+    created_time = models.DateTimeField('评论时间', auto_now_add = True)
     question = models.ForeignKey(Question, on_delete = models.PROTECT)
 
     def __str__(self):
-        return self.author.__str__() + '在问题“' + self.question.__str__() + '”下的评论'
+        return self.author.__str__() + ' 在问题“' + self.question.__str__() + '”下的评论'
 
     class Meta():
         verbose_name_plural = 'CommentsOfQuestions'
 
 class CommentOfAnswer(models.Model):
-    content = models.TextField(_('评论内容'),)
+    content = models.TextField('评论内容')
     author = models.ForeignKey('users.User', on_delete = models.PROTECT)
-    created_time = models.DateTimeField(_('评论时间'), auto_now_add = True)
+    created_time = models.DateTimeField('评论时间', auto_now_add = True)
     answer = models.ForeignKey(Answer, on_delete = models.PROTECT)
 
     def __str__(self):
-        return self.author.__str__() + '在回答”' + self.answer.__str__() + '”下的评论'
+        return self.author.__str__() + ' 在回答”' + self.answer.__str__() + '”下的评论'
 
     class Meta():
         verbose_name_plural = 'CommentsOfAnswers'
 
 class QuestionStatus(models.Model):
-    able_modify = models.BooleanField(_('可编辑'), default = True)
-    able_answer = models.BooleanField(_('可添加回答'), default = True)
+    able_modify = models.BooleanField('可编辑', default = True)
+    able_answer = models.BooleanField('可添加回答', default = True)
     question = models.OneToOneField(Question, on_delete = models.PROTECT)
 
     def __str__(self):
--- back_end/users/models.py
+++ back_end/users/models.py
@@ -1,45 +1,86 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from .functions import user_avatar_path
 
 # Create your models here.
 
+gender_choices = (
+    (0, '男'),
+    (1, '女'),
+    (2, '保密')
+)
+
+condition_choices = (
+    (0, '本科在读'),
+    (1, '硕士在读'),
+    (2, '博士在读'),
+    (3, '毕业')
+)
+
 class User(AbstractUser):
     username_validator = UnicodeUsernameValidator()
 
     username = models.CharField(
-        _('学号'),
+        '学号',
         max_length = 20,
         unique = True,
-        help_text=_('学号即用户名，账号由管理员预设置'),
+        help_text='学号即用户名，账号由管理员预设置',
         validators = [username_validator],
         error_messages={
-            'unique': _('学号不能重复！'),
+            'unique': '学号不能重复！',
             },
         )
     #在注册账号时管理员将用户名置为学号
     #nickname是用户名，username是学号
-    nickname = models.CharField(_('用户名'), max_length = 20, blank = True)
-    avatar = models.ImageField(_('头像'), upload_to = user_avatar_path, blank = True, null = True)
+    nickname = models.CharField('用户名', max_length = 20, blank = True)
+    avatar = models.ImageField('头像', upload_to = user_avatar_path, blank = True, null = True)
+    mylist = models.ManyToManyField('content.Answer', related_name = '收藏夹')
+    history = models.ManyToManyField('content.Answer', related_name = '历史记录')
     
     class Meta(AbstractUser.Meta):
         pass
 
 class Profile(models.Model):
-    #个人资料有啥以后再说
     user = models.OneToOneField(User, on_delete = models.PROTECT, default = None)
+
+    gender = models.SmallIntegerField('性别', choices = gender_choices, default = 2)
+    birthday = models.DateField('生日', blank = True)
+    school = models.CharField('所属学校', max_length = 20, blank = True)
+    major = models.CharField('专业', max_length = 20, blank = True)
+    condition = models.SmallIntegerField('在读情况', choices = condition_choices, blank = True)
 
     def __str__(self):
         return self.user.username + '用户资料'
 
 class Status(models.Model):
     user = models.OneToOneField(User, on_delete = models.PROTECT, default = None)
-    if_banned = models.BooleanField(_('被封禁'), default = False)
+    if_banned = models.BooleanField('被封禁', default = False)
 
     def __str__(self):
         return self.user.username + '用户状态'
 
     class Meta():
         verbose_name_plural = 'Status'
+
+class MylistInfo:
+    user = models.ForeignKey(User, on_delete = models.PROTECT)
+    answer = models.ForeignKey('content.Answer', on_delete = models.PROTECT)
+    add_time = models.DateTimeField('收藏时间', auto_now_add = True)
+
+    def __str__(self):
+        return self.user.username + '收藏”' + self.answer.__str__() + '“信息'
+
+    class Meta():
+        verbose_name_plural = 'MylistInfo'
+
+class HistoryInfo:
+    user = models.ForeignKey(User, on_delete = models.PROTECT)
+    answer = models.ForeignKey('content.Answer', on_delete = models.PROTECT)
+    add_time = models.DateTimeField('最后访问', auto_now_add = True)
+
+    def __str__(self):
+        return self.user.username + '历史记录”' + self.answer.__str__() + '“信息'
+
+    class Meta():
+        verbose_name_plural = 'HistoryInfo'
